c1d93dd7fea9e628f4939715f17f9bd114af9c68
Mrunal401 <52656959+Mrunal401@users.noreply.github.com>
2025-03-09 23:30:43 -0700

    completed Exercise_4.java

--- Exercise_4.java
+++ Exercise_4.java
@@ -1,3 +1,6 @@
+//time complexity = O(nlogn)
+//space complexity = O(n)
+
 class MergeSort 
 { 
     // Merges two subarrays of arr[]. 
@@ -6,6 +9,46 @@ class MergeSort
     void merge(int arr[], int l, int m, int r) 
     {  
        //Your code here  
+	    int n1 = m - l + 1;
+        int n2 = r - m;
+
+        // Create temporary arrays
+        int L[] = new int [n1];
+        int R[] = new int [n2];
+
+        // Copy data to temporary arrays
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        // Merge the temporary arrays back into arr[l..r]
+        int i = 0, j = 0;
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy any remaining elements of L[] (if any)
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        // Copy any remaining elements of R[] (if any)
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
     } 
   
     // Main function that sorts arr[l..r] using 
@@ -14,6 +57,17 @@ class MergeSort
     { 
 	//Write your code here
         //Call mergeSort from here 
+	    if (l < r) {
+            // Find the middle point
+            int m = (l + r) / 2;
+
+            // Recursively sort the first and second halves
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
     } 
   
     /* A utility function to print array of size n */
