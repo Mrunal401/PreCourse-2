e7ccbf8f4669934f4e9720b5454494acc158b6a3
Mrunal401 <52656959+Mrunal401@users.noreply.github.com>
2025-03-09 23:21:53 -0700

    completed Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,8 +1,29 @@
+//Time complexity = O(logN)
+//Space complexity = O(1)
 class BinarySearch { 
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
         //Write your code here
+        int mid;
+        while(l<=r)
+        {
+            mid=l + (r - l)/2;
+            //mid=(l + r)/2;
+            if(arr[mid]==x)
+            {
+                return mid;
+            }
+            if(x>arr[mid])
+            {
+                l=mid + 1;
+            }
+            else
+            {
+                r=mid - 1;
+            }
+        }
+        return -1;
     } 
   
     // Driver method to test above 
